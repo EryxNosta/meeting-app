5d33599e3fb026921973c6c9adabd8b4e691f346
Silvia Valderramos <cvalderramos@gmail.com>
2023-07-31 00:24:55 -0600

    intento 2 de que aparezca texto de navbar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,33 +11,31 @@
     <title>Signin</title>      
 </head>
 
-<body class="">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark navyb">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.html">Meeting APP</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Organizar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Eventos</a>
-                    </li>
-                </ul>
-                <nav class="navbar bg-navbar">
-                    <div class="container-fluid">
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-primary" type="submit">Search</button>
-                        </form>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.html">Meeting APP</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Organizar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Eventos</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-primary" type="submit">Search</button>
+                    </form>
+                    <div class="d-flex justify-content-end mt-2 mr-3">
+                        <button type="button" class="btn btn-primary me-2">Registrarse</button>
                     </div>
-                </nav>           
-                
+                </div>
             </div>
-        </div>
         </nav>  
         <x-guest-layout>
             <!-- Session Status -->
@@ -101,6 +99,8 @@
           <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary redc">About</a></li>
         </ul>
     </footer>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHkM9GAe6s3Gfu6zJP5E1iKfg5V8P1BHb8Hs1gezFksPe3jGp2KZ8CY1D" crossorigin="anonymous"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
